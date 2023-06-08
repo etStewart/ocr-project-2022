1215c91883468ea5dca6e453a6cb9408b37b7a3e
etStewart <113509260+etStewart@users.noreply.github.com>
2023-06-08 09:57:57 +0100

    Changing files

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,14 +1,18 @@
-const Alpaca = require('@alpacahq/alpaca-trade-api');  //import alpaca API package I installed via NPM
+import Alpaca from '/Users/ethan/node_modules/@alpacahq/alpaca-trade-api';  //import alpaca API package I installed via NPM
+
+//assinging API keys to veriables
+const APIKey = "PKUEDSU4AC0X6V7YX0DG";
+const APISecret = "a67GoahEfGkQNLIhxAMArTYpyOqgy49zaFWbxzQl"
 
 //object used to access different API requests
 const Alpaca = new Alpaca({
-    keyID: environment.env.APIKey,
-    secretID: environment.env.APISecret,
+    keyID: APIKey,
+    secretID: APISecret,
     paper: true
 });
 
 async function printAccount() {
-    const account = await alpaca.getAccount();
+    const account = await account();
     console.log(account);
 }
 
