c64b0b2129128525a578c54309c9bef2be35dc48
etStewart <113509260+etStewart@users.noreply.github.com>
2023-06-14 09:17:37 +0100

    minor unnoticeable change

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+//fetching the alpaca API
 const Alpaca = require('@alpacahq/alpaca-trade-api')
 
+//keys for project
 const APIKey = "PKQU3UMD89C6VSJGZRAV"
 const APISecret = "IsaGQPd9bhv4nH5ahSJO4MvSNKPKXiG2RHZpAbyK"
 
+//using object to store necessary data
 const alpaca = new Alpaca({
     keyId: APIKey,
     secretKey: APISecret,
     paper: true
   });
 
-  async function printAccount() {
+//using an asynchornis function to call the data from my alpaca account
+async function printAccount() {
     const account = await alpaca.getAccount();
     console.log(account);
-  }
+}
 
   printAccount();
 
