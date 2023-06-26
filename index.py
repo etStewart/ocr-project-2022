35b07d34d38f0119365a653110631cc24231ee50
etStewart <113509260+etStewart@users.noreply.github.com>
2023-06-26 09:30:23 +0100

    Adding detail to python file

--- index.py
+++ index.py
@@ -1,14 +1,5 @@
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockLatestQuoteRequest
-
-async def get_account_info():
-    print("Hello World")
-
-async def get_last_bars():
-    print("Hello World")
-
-async def get_realtime_price():
-    print("Hello World")
     
 API_KEY = "PKBFRFWBV2IIXYC9BPQ5"
 SECRET_KEY = "9rZHrcqHyVAqh6TrljCTayCfghqTmY9HRpkBK7uE"
@@ -20,4 +11,21 @@ multisymbol_request_params = StockLatestQuoteRequest(symbol_or_symbols=["SPY", "
 
 latest_multisymbol_qoutes = stock_client.get_stock_latest_quote(multisymbol_request_params)
 
-a
+gld_latest_ask_price = latest_multisymbol_qoutes["GLD"].ask_price
+spy_latest_ask_price = latest_multisymbol_qoutes["SPY"].ask_price
+tlt_latest_ask_price = latest_multisymbol_qoutes["TLT"].ask_price
+
+print("The current price of TLT is " + str(tlt_latest_ask_price))
+print("The current price of SPY is " + str(spy_latest_ask_price))
+print("The current price of GLD is " + str(gld_latest_ask_price))
+
+"""
+async def get_account_info():
+    print("Hello World")
+
+async def get_last_bars():
+    print("Hello World")
+
+async def get_realtime_price():
+    print("Hello World")
+"""
