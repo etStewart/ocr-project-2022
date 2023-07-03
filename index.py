daa90ed6c252b291d7e425fdb26bfc412bf3b64c
etStewart <113509260+etStewart@users.noreply.github.com>
2023-07-03 08:55:14 +0000

    Ensuring program works in codespace

--- index.py
+++ index.py
@@ -1,21 +1,21 @@
+from datetime import datetime
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockBarsRequest
 from alpaca.trading.client import TradingClient
 from alpaca.data.timeframe import TimeFrame
-from datetime import datetime
 
-sma20 = 0
-sma50 = 0
-    
+SMA_20 = 0
+SMA_50 = 0
+   
 API_KEY = "PKBFRFWBV2IIXYC9BPQ5"
 SECRET_KEY = "9rZHrcqHyVAqh6TrljCTayCfghqTmY9HRpkBK7uE"
 
 trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
 stock_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
 
-def GET_ACCOUNT_INFO():
-    ACCOUNT = trading_client.get_account()
-    return ACCOUNT
+def get_account_info():
+    account = trading_client.get_account()
+    return account
 
 def initialize_average():
     now = datetime.datetime.now()
@@ -24,15 +24,12 @@ def initialize_average():
     initial_data = StockBarsRequest(
         symbol_or_symbols=["SPY", "GLD"],
         timeframe=TimeFrame.Minute,
-        start=datetime(2022, 7, 1),
-        end=datetime(2022, 9, 1)
+        start=time_50_minutes_ago,
+        end=now
     )
 
-    bars = stock_client.get_stock_bars(initial_data)
-    panda_bars = bars.df
+print(get_account_info())
 
-    print(last_minute_time)
 
-initialize_average()
 
     
