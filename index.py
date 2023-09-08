7678adbc1473f2ee1a585bcec7fcc6a7454fd510
etStewart <113509260+etStewart@users.noreply.github.com>
2023-09-08 15:07:52 +0100

    Developing the new system

--- index.py
+++ index.py
@@ -1,17 +1,30 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 # Importing necessary libraries
 import backtrader as bt
+import datetime
+import os.path
+import sys
 
 if __name__ == '__main__':
     # Create a cerebro instance
-    cerebro = bt.Cerebro()
-    cerebro.broker.setcash(100000.0)
+    account = bt.Cerebro()
+    account.broker.setcash(100000.0)
+
+    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    datapath = os.path.join(modpath, '../../datas/orcl-1995-2014.txt')
+
+    datafeed = bt.feed.YahooFinanceCSVData(
+        dataname = datapath,
+        fromdate = datetime.datetime(2000, 1, 1),
+        todate = datetime.datetime(2000, 12, 1),
+        reverse = False
+    )
 
     # Print the starting portfolio value
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    print('Starting Portfolio Value: %.2f' % account.broker.getvalue())
 
     # Run the strategy
-    cerebro.run()
+    account.run()
 
     # Print the final portfolio value
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    print('Final Portfolio Value: %.2f' % account.broker.getvalue())
