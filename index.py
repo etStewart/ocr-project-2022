9a774bd50c34d9e07dd89d65aab30e234db8f940
etStewart <113509260+etStewart@users.noreply.github.com>
2023-09-14 09:14:56 +0100

    Linking data feed to account

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
-# Importing necessary libraries
+
 import backtrader as bt
 import datetime
 import os.path
 import sys
 
 if __name__ == '__main__':
-    # Create a cerebro instance
+
     account = bt.Cerebro()
     account.broker.setcash(100000.0)
 
@@ -20,11 +20,10 @@ if __name__ == '__main__':
         reverse = False
     )
 
-    # Print the starting portfolio value
+    account.adddata(datafeed)
+
     print('Starting Portfolio Value: %.2f' % account.broker.getvalue())
 
-    # Run the strategy
     account.run()
 
-    # Print the final portfolio value
     print('Final Portfolio Value: %.2f' % account.broker.getvalue())
