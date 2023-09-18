88afd15dbb82561b242f210927a807c0a1c31a22
etStewart <113509260+etStewart@users.noreply.github.com>
2023-09-18 12:17:26 +0100

    SMA implement and orcl csv

--- index.py
+++ index.py
@@ -7,23 +7,67 @@ import sys
 
 import backtrader as bt
 
+
 class Strategy(bt.Strategy):
 
+    params = dict(
+        fast_period=10,
+        slow_period=30
+    )
+
+    def __init__(self):
+        sma1 = bt.ind.SMA(period=self.p.fast_period)
+        sma2 = bt.ind.SMA(period=self.p.slow_period)
+        self.crossover = bt.ind.CrossOver(sma1, sma2)
+
+    def next(self):
+        if not self.position:
+            if self.crossover > 0:
+                self.buy()
+
+        elif self.crossover < 0:
+            self.close()
+
+
+account = bt.Cerebro()
+account.addstrategy(Strategy)
+
+data = bt.feeds.YahooFinanceCSVData(
+    dataname='orcl.csv',
+    fromdate=datetime.datetime(1996, 1, 1),
+    todate=datetime.datetime(2014, 12, 31),
+    reverse=None)
+
+account.adddata(data)
+
+account.broker.setcash(100000.0)
+
+print('Starting Portfolio Value: %.2f' % account.broker.getvalue())
+
+account.run()
+
+print('Final Portfolio Value: %.2f' % account.broker.getvalue())
+
+
+'''
     def log(self, txt, dt=None):
         dt = dt or self.datas[0].datetime.date(0)
         print('%s, %s' % (dt.isoformat(), txt))
+'''
 
-    def __init__(self):
-        self.dataclose = self.datas[0].close
-        self.order = None
 
+'''
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
             return
 
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log('BUY EXECUTED, %.2f' % order.executed.price)
+                self.log('BUY EXECUTED, %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+                self.buyprice = order.executed.price
             elif order.issell():
                 self.log('SELL EXECUTED, %.2f' % order.executed.price)
             self.bar_executed = len(self)
@@ -32,43 +76,10 @@ class Strategy(bt.Strategy):
             self.log('Order Canceled/Margin/Rejected')
 
         self.order = None
+'''
 
-    def next(self):
-        self.log('Close, %.2f' % self.dataclose[0])
 
-        if self.order:
-            return
-
-        if not self.position:
-            if self.dataclose[0] < self.dataclose[-1]:
-                if self.dataclose[-1] < self.dataclose[-2]:
-                    self.log('BUY CREATE, %.2f' % self.dataclose[0])
-                    self.order = self.buy()
-
-        else:
-            if len(self) >= (self.bar_executed + 5):
-                self.log('SELL CREATE, %.2f' % self.dataclose[0])
-                self.order = self.sell()
-
-if __name__ == '__main__':
-    cerebro = bt.Cerebro()
-    cerebro.addstrategy(Strategy)
-
+'''
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
     datapath = os.path.join(modpath, '../ocr-project-2022/orcl-1995-2014.txt')
-
-    data = bt.feeds.YahooFinanceCSVData(
-        dataname=datapath,
-        fromdate=datetime.datetime(2000, 1, 1),
-        todate=datetime.datetime(2000, 12, 31),
-        reverse=False)
-
-    cerebro.adddata(data)
-
-    cerebro.broker.setcash(100000.0)
-
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-
-    cerebro.run()
-
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+'''
